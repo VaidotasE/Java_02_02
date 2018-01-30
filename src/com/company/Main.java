549bb3b84c19f69e29d5a679d6a2068a6ffb4c72
Vaidotas <vaidotas.eigelis@gmail.com>
2018-01-30 14:57:02 +0200

    Code 02

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Irasyk varda: ");
+        String vardas = sc.nextLine();
+        int ilgis = vardas.length();
+
+        System.out.println("Vardas: " + vardas);
+        System.out.println("Vardo ilgis: " + ilgis);
     }
 }
